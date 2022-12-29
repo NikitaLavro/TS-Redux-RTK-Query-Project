775ad8319ae2147408497e8c51a932bd778421b4
Nikita <lavro.nikita123@gmail.com>
2022-12-29 17:58:26 -0500

    fetchUsers logic

--- redux-ts-rtk/src/Store/Reducers/UserSlice.ts
+++ redux-ts-rtk/src/Store/Reducers/UserSlice.ts
@@ -18,7 +18,7 @@ export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    usersFetching(state, action) {
+    usersFetching(state) {
       state.isLoading = true;
     },
     usersFetchingSuccess(state, action: PayloadAction<IUser[]>) {
--- redux-ts-rtk/src/Store/reducers/ActionCreators.ts
+++ redux-ts-rtk/src/Store/reducers/ActionCreators.ts
@@ -1,11 +1,17 @@
 import axios from "axios";
+import { InstanceOf } from "reselect/es/types";
 import { IUser } from "../models/IUser";
 import { AppDispatch } from "../Store";
+import { userSlice } from "./UserSlice";
 
 export const fetchUsers = () => async (dispatch: AppDispatch) => {
   try {
-    const response = await axios.get<IUser>(
+    dispatch(userSlice.actions.usersFetching());
+    const response = await axios.get<IUser[]>(
       "https://jsonplaceholder.typicode.com/users"
     );
-  } catch (error) {}
+    dispatch(userSlice.actions.usersFetchingSuccess(response.data));
+  } catch (error: any) {
+    dispatch(userSlice.actions.usersFetchingError(error.message));
+  }
 };
