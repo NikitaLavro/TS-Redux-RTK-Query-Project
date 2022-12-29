d591244c0001f0c93d9b3123b6e3968e907b492f
Nikita <lavro.nikita123@gmail.com>
2022-12-29 17:33:50 -0500

    Increment reducer implementation

--- redux-ts-rtk/src/App.tsx
+++ redux-ts-rtk/src/App.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./App.css";
 
+//Hooks
+import { useAppDispatch, useAppSelector } from "./Store/hooks/redux";
+import { userSlice } from "./Store/reducers/UserSlice";
+
 function App() {
-  return <div className="App">App</div>;
+  const { count } = useAppSelector((state) => state.userReducer);
+  const { increment } = userSlice.actions;
+
+  const dispatch = useAppDispatch;
+
+  return (
+    <div className="App">
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(increment())}>Inccrement</button>
+    </div>
+  );
 }
 
 export default App;
