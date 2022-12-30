6b5b6cd80fb7047c8e0f273d5658d7fecea9f849
Nikita <lavro.nikita123@gmail.com>
2022-12-29 19:36:12 -0500

    refetch logic

--- redux-ts-rtk/src/components/PostContainer.tsx
+++ redux-ts-rtk/src/components/PostContainer.tsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { postAPI } from "../Store/services/PostService";
 
 import PostItem from "./PostItem";
 
 const PostContainer = () => {
-  const { data: posts } = postAPI.useFetchAllPostsQuery(5);
+  const [limit, setLimit] = useState(5);
+  const {
+    data: posts,
+    error,
+    isLoading,
+    refetch,
+  } = postAPI.useFetchAllPostsQuery(limit);
+
+  useEffect(() => {}, []);
 
   return (
     <div>
       <div className="post__list">
+        <button onClick={() => refetch()}>Refetch</button>
+        {isLoading && <h1>Loading...</h1>}
+        {error && <h1>Something went wrong...</h1>}
         {posts && posts.map((post) => <PostItem key={post.id} post={post} />)}
       </div>
     </div>
