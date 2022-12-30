b056e2fcf28082a6e47219600091c38074a1b61b
Nikita <lavro.nikita123@gmail.com>
2022-12-29 20:41:11 -0500

    update and delete fuctionality

--- redux-ts-rtk/src/components/PostContainer.tsx
+++ redux-ts-rtk/src/components/PostContainer.tsx
@@ -13,10 +13,20 @@ const PostContainer = () => {
     refetch,
   } = postAPI.useFetchAllPostsQuery(limit);
   const [createPost, {}] = postAPI.useCreatePostMutation();
+  const [updatePost, {}] = postAPI.useUpdatePostMutation();
+  const [deletePost, {}] = postAPI.useDeletePostMutation();
 
   const handleCreate = async () => {
     const title = prompt();
     await createPost({ title, body: title } as IPost);
+  };
+
+  const handleRemove = (post: IPost) => {
+    deletePost(post);
+  };
+
+  const handleUpdate = (post: IPost) => {
+    updatePost(post);
   };
 
   return (
@@ -25,7 +35,15 @@ const PostContainer = () => {
         <button onClick={() => handleCreate()}>Add new post</button>
         {isLoading && <h1>Loading...</h1>}
         {error && <h1>Something went wrong...</h1>}
-        {posts && posts.map((post) => <PostItem key={post.id} post={post} />)}
+        {posts &&
+          posts.map((post) => (
+            <PostItem
+              remove={handleRemove}
+              update={handleUpdate}
+              key={post.id}
+              post={post}
+            />
+          ))}
       </div>
     </div>
   );
--- redux-ts-rtk/src/components/PostItem.tsx
+++ redux-ts-rtk/src/components/PostItem.tsx
@@ -3,14 +3,29 @@ import { IPost } from "../Store/models/IPost";
 
 interface PostItemProps {
   post: IPost;
+  remove: (post: IPost) => void;
+  update: (post: IPost) => void;
 }
 
-const PostItem: FC<PostItemProps> = ({ post }) => {
+const PostItem: FC<PostItemProps> = ({ post, remove, update }) => {
+  const handleRemove = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    remove(post);
+  };
+
   return (
-    <div className="post">
-      {post.id}. {post.title}
-      <button>Delete</button>
-    </div>
+    <>
+      <div
+        className="post"
+        onClick={(e: React.MouseEvent) => {
+          const title = prompt() || "";
+          update({ ...post, title });
+        }}
+      >
+        {post.id}. {post.title}
+      </div>
+      <button onClick={() => handleRemove}>Delete</button>
+    </>
   );
 };
 
